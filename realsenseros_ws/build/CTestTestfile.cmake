5dbe93e307f3f350e25afda650b244a00d3e3413
Marco-Yang <3155853275@qq.com>
2023-04-23 22:08:18 +0800

    realsensed455

---
+++ realsenseros_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/noetic/realsenseros_ws/src
+# Build directory: /home/noetic/realsenseros_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("realsense-ros/realsense2_description")
+subdirs("realsense-ros/realsense2_camera")
